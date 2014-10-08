2a95f5abd3cb077d447337e4e6a400b54c47c5e0
Joe Schneider <josschne@gmail.com>
2014-10-08 01:35:36 -0500

    Fixed a few issues related to card manipulation

--- main.js
+++ main.js
@@ -44,27 +44,25 @@ function playCommon(card, p) {
 }
 
 function playBase(card, p) {
-	Readline.question('Base playing');
 	playCommon(card, p);
 }
 
 function playCard(card, p) {
 	playCommon(card, p);
-	if (card.hasOwnProperty('base')) { Readline.question('Base running'); }
-	if (card.hasOwnProperty('base') || card.hasOwnProperty('outpost')) { process.exit(1); moveCard(card, p.hand, p.bases); }
+	if (card.hasOwnProperty('base') || card.hasOwnProperty('outpost')) { moveCard(card, p.hand, p.bases); }
 }
 
 function moveCard(card, src, dst) {
-	dst = dst.concat(card);
+	dst.push(card);
 	src.splice(src.indexOf(card), 1);
 }
 
 function play(p, notp, trade) {
-	console.log(p.name, "'s turn!");
+	//console.log(p.name, "'s turn!");
 
 	//Main
-	console.log("HAND: ", p.hand.map(function(card) { return card.name; }));
-	console.log("BASES: ", p.bases.map(function(card) { return card.name; }));
+	//console.log("HAND: ", p.hand.map(function(card) { return card.name; }));
+	//console.log("BASES: ", p.bases.map(function(card) { return card.name; }));
 
 	p.bases.forEach(function(card) {
 		playBase(card, p);
@@ -73,9 +71,9 @@ function play(p, notp, trade) {
 		playCard(card, p);
 	});
 
-	console.log("T:", p.trade, "A:", p.authority, "C:", p.combat);
+	//console.log("T:", p.trade, "A:", p.authority, "C:", p.combat);
 	
-	console.log("TRADE: ", trade.row.map(function(card) { return card.name; }));
+	//console.log("TRADE: ", trade.row.map(function(card) { return card.name; }));
 	
 	//Main Combat
 	while (p.combat > 0) {
@@ -96,20 +94,20 @@ function play(p, notp, trade) {
 			}
 		}
 
+		//console.log(p.name, " attacks ", notp.name, " for ", p.combat, " authority.");
 		notp.authority -= p.combat;
 		p.combat = 0;
-		console.log(p.name, " attacks ", notp.name, " for ", p.combat, " authority.");
 	}
 
 
 	//Main Trade
 	var toBuy = trade.row.filter(function(card) { return (card.cost <= p.trade)});
-	while(toBuy.length > 0)
+	while(toBuy.length > 0 && trade.deck.length > 0)
 	{
-		console.log(toBuy.map(function(card) { return card.name; }));
-		console.log(p.name, " trades ", toBuy[0].cost, " for ", toBuy[0].name);
+		//console.log(toBuy.map(function(card) { return card.name; }));
+		//console.log(p.name, " trades ", toBuy[0].cost, " for ", toBuy[0].name);
 		moveCard(toBuy[0], trade.row, p.discard);
-		trade.row = trade.row.concat(trade.deck.draw(1));
+		trade.row.push(trade.deck.draw(1));
 		p.trade -= toBuy[0].cost;
 		toBuy = trade.row.filter(function(card) { return (card.cost <= p.trade)});
 	}
@@ -154,7 +152,7 @@ function runGame()
 	return [p1.authority, p2.authority];
 }
 
-var simulationCount = 1;
+var simulationCount = 1000;
 var winCount = [0, 0];
 for (var s=0; s<simulationCount; s++) {
 	var result = runGame();
